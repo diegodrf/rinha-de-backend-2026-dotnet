31baf2f69f4fc182c1ab4f9a4178138a0e50827b
Diego Faria <diego.rdfaria@gmail.com>
2026-04-28 17:09:56 -0400

    apply timetout control

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,11 +1,13 @@
 using System.Text.Json;
+using Microsoft.AspNetCore.Http.Timeouts;
+using WebApi;
 using WebApi.DTOs;
 using WebApi.Repositories;
 using WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(op =>
+builder.Services.AddDbContextPool<AppDbContext>(op =>
 {
     var connectionString = builder.Configuration.GetConnectionString("rinha-db");
     ArgumentException.ThrowIfNullOrWhiteSpace(connectionString);
@@ -15,6 +17,20 @@ builder.Services.AddDbContext<AppDbContext>(op =>
 
 builder.Services.AddScoped<IAntifraudRepository, AntifraudRepository>();
 builder.Services.AddScoped<IAntifraudService, AntifraudService>();
+builder.Services.AddRequestTimeouts(c =>
+{
+    c.AddPolicy("Policy", new RequestTimeoutPolicy
+    {
+        Timeout = TimeSpan.FromMilliseconds(Constants.DatabaseTargetResponseTimeInMilliseconds),
+        TimeoutStatusCode = StatusCodes.Status200OK,
+        WriteTimeoutResponse = async (context) =>
+        {
+            context.Response.ContentType = "application/json";
+            var json = JsonSerializer.Serialize(new TransactionResponseDto() { Approved = false, FraudScore = 1f });
+            await context.Response.WriteAsync(json);
+        }
+    });
+});
 
 builder.Services.ConfigureHttpJsonOptions(op =>
 {
@@ -22,6 +38,7 @@ builder.Services.ConfigureHttpJsonOptions(op =>
 });
 
 var app = builder.Build();
+app.UseRequestTimeouts();
 
 app.MapGet("/ready", async Task<IResult> (
         IAntifraudService service,
@@ -41,7 +58,8 @@ app.MapPost("/fraud-score", async Task<TransactionResponseDto> (
     TransactionRequestDto dto,
     IAntifraudService service,
     CancellationToken cancellationToken
-    ) => await service.GetScoreAsync(dto, cancellationToken));
+    ) => await service.GetScoreAsync(dto, cancellationToken))
+    .WithRequestTimeout("Policy");
 
 app.Run();
 
